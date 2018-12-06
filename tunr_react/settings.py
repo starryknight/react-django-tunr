8d08953d8258e5797099f640196a03ea668ca407
stanley kimani <stanleykimani@stanleys-MacBook-Air.local>
2018-12-06 11:01:02 -0500

    set up dependencies and heroku

--- tunr_react/settings.py
+++ tunr_react/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'tunr_react.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'react_django_tunr',
+        'USER': 'tunr_admin',
+        'PASSWORD': 'tunr_admin',
+        'HOST': 'localhost'
     }
 }
 
@@ -118,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Configure Django App for Heroku.
+  import django_heroku
+
+  django_heroku.settings(locals())
